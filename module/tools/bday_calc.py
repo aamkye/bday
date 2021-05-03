190c442dcba0353f4e96a2cc56a4f2c9d25e6e3c
Amadeusz Kryze <amadeusz.kryze@gmail.com>
2021-05-04 01:08:02 +0200

    fixed init setup in function

--- module/tools/bday_calc.py
+++ module/tools/bday_calc.py
@@ -1,6 +1,8 @@
 import datetime
 
-def calculate_dates(bday: datetime.date, now: datetime.date = datetime.date.today()):
+def calculate_dates(bday: datetime.date, now: datetime.date = None):
+    if now is None:
+        now = datetime.date.today()
     delta1 = datetime.datetime(now.year, bday.month, bday.day).date()
     delta2 = datetime.datetime(now.year+1, bday.month, bday.day).date()
 
